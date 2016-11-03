074add7a943ca3512ac6056bf7c1c31f049ff5a1
shenyunlong <shenyunlong@qiyi.com>
2016-11-03 19:41:12 +0800

    A Simple Web Server - v2

--- simple_web_server.py
+++ simple_web_server.py
@@ -1,4 +1,5 @@
 import http.server
+import os
 
 class MyRequestHandler(http.server.BaseHTTPRequestHandler):
     Page = '''
@@ -16,10 +17,32 @@ class MyRequestHandler(http.server.BaseHTTPRequestHandler):
     </html>
     '''
 
+    Error_Page = '''
+    <html>
+    <body>
+    <h1>Error accessing {path}</h1>
+    <p>{msg}</p>
+    </body>
+    </html>
+    '''
+
     def do_GET(self):
         #print(self.Page)
-        page = self.create_page()
-        self.send_page(page)
+        #page = self.create_page()
+        #self.send_page(page)
+        try:
+            path = os.getcwd() + self.path
+
+            if not os.path.exists(path):
+                #self.handle_error("{0} not found".format(self.path))
+                raise ServerException("{0} not found".format(self.path))
+            elif os.path.isfile(path):
+                self.handle_file(path)
+            else:
+                #self.handle_error("Unknown object {0}".format(self.path))
+                raise ServerException("Unknown object {0}".format(self.path))
+        except Exception as msg:
+            self.handle_error(msg)
 
     def create_page(self):
         values = {
@@ -39,7 +62,25 @@ class MyRequestHandler(http.server.BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(bytes(page, "utf-8"))
 
+    def handle_file(self, path):
+        try:
+            with open(path, 'r') as reader:
+                # can be optimize here
+                content = reader.read()
+            self.send_page(content)
+        except IOError as msg:
+            self.handle_error("{0} can not be read {1}".format(self.path, msg))
+
+    def handle_error(self, msg):
+        values = {
+            'path' : self.path,
+            'msg' : msg
+        }
+        page = self.Error_Page.format(**values)
+        self.send_page(page)
+
 if __name__ == '__main__':
-    server_address = ('', 8080) #tuple
+    server_address = ('', 8080) # tuple
     http_server = http.server.HTTPServer(server_address, MyRequestHandler)
     http_server.serve_forever()
+
